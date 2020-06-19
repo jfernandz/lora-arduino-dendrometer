b4982896dac797c3df83fe77b358b0297518cd3c
Javier Fernández <WyRe12@gmail.com>
2020-06-19 16:51:12 +0200

    Varies the TX_INTERVAL

--- testing/Arduino/dendro/dendro.ino
+++ testing/Arduino/dendro/dendro.ino
@@ -56,7 +56,7 @@ static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 10;
+const unsigned TX_INTERVAL = 60;
 
 // Pin mapping
 const lmic_pinmap lmic_pins = {
