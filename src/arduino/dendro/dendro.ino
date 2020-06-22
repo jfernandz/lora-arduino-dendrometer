675fd0bda6b6467f46f7aa36142c74c962f6bc37
Javier Fernández <WyRe12@gmail.com>
2020-06-22 22:33:32 +0200

    Completes docs

--- src/arduino/dendro/dendro.ino
+++ src/arduino/dendro/dendro.ino
@@ -71,7 +71,7 @@ double potVal = 0;             // Potmeter value
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 10;
+const unsigned TX_INTERVAL = 60;
 
 // Pin mapping
 const lmic_pinmap lmic_pins = {
