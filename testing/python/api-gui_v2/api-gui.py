dfbcba2498178fb49604dae1e0659fb09ad696d2
Javier Fernández <WyRe12@gmail.com>
2020-06-28 13:59:58 +0200

    Improves api-guy.py

--- testing/python/api-gui_v2/api-gui.py
+++ testing/python/api-gui_v2/api-gui.py
@@ -1,29 +1,13 @@
 import requests
 import tkinter as tk
+from tkinter import ttk
+from string import hexdigits
+
 import json
-
-
-def use_api(url, method=requests.get, header=None, body=None):
-    body = json.dumps(body) if body else None
-
-    response = method(url, headers=header, data=body)
-    print(url, method, response.status_code, response.text)
-
-    return response
-
 
 # These are customizable patterns, actually
 fiware_service = 'atosioe'
 fiware_servicepath = '/lorattn'
-
-# These are application unique
-TTN_app_id = 'dendrometer'
-TTN_app_pw = 'ttn-account-v2.173lH8wwDiIRC8E2JgM9ScXyuNRlPpMefpazS0TIhnU'
-TTN_app_eui = '70B3D57ED0030B5D'
-
-# These are device unique
-TTN_app_skey = '7AF6B9A29EAF893F66C0F4360BA8DD5B'
-TTN_dev_eui = '00EC51D264A6F8FD'
 
 header_iot_device = {
     "Content-Type": "application/json",
@@ -38,6 +22,19 @@ header_orion = {
 }
 
 
+def use_api(url, method=requests.get, header=None, body=None):
+    body = json.dumps(body) if body else None
+
+    response = method(url, headers=header, data=body)
+    # print(url, method, response.status_code, response.text)
+
+    return response
+
+
+def is_hex(string) -> bool:
+    return all([char in hexdigits for char in string])
+
+
 class Application(tk.Frame):
     entities = list()
     default_entity = 'LORA-N-0'
@@ -47,56 +44,169 @@ class Application(tk.Frame):
 
     subscriptions = list()
 
+    # These are application unique
+    TTN_app_id = None
+    TTN_app_pw = None
+    TTN_app_eui = None
+
+    # These are device unique
+    TTN_app_skey = None
+    TTN_dev_eui = None
+
     def __init__(self):
+        # TKINTER constructor
         self.gui = tk.Tk()
         self.gui.title('Fiware Client')
         super().__init__(self.gui)
-        self.gui.geometry("640x480")
+        self.gui.minsize(640, 480)
+        # self.gui.geometry("640x480")
         self.pack()
-        main_frame = tk.Frame(self.gui)
-        main_frame.pack(fill=tk.BOTH, expand=tk.YES)
 
-        self.label_orion_ver = tk.Label(main_frame, text='Orion version: ' + use_api('http://localhost:1026/version')
+        # TABS
+        self.notebook = ttk.Notebook(self.gui)  # Tab controller
+        self.tab_app_keys = ttk.Frame(self.notebook)
+        self.tab_devices_management = ttk.Frame(self.notebook)
+        self.tab_about = ttk.Frame(self.notebook)
+        self.notebook.add(self.tab_app_keys, text='Applications Keys')
+        self.notebook.add(self.tab_devices_management, text='Devices Management')
+        self.notebook.add(self.tab_about, text='About')
+        self.notebook.tab(1, state='disabled')  # disabled by default until the app data is introduced
+        self.notebook.pack(expand=1, fill="both")
+
+        # Widgets
+        self.set_devices_management_widgets()
+        self.set_about_widgets()
+        self.set_applications_keys_widgets()
+
+        #
+        self.api_get_devices_and_entities()
+
+    def set_about_widgets(self) -> None:
+        self.label_orion_ver = tk.Label(self.tab_about,
+                                        text='Orion version: ' + use_api('http://localhost:1026/version')
                                         .json()['orion']['version'])
-        self.label_iot_ver = tk.Label(main_frame, text='IoT version: ' + use_api('http://localhost:4061/iot/about')
+        self.label_iot_ver = tk.Label(self.tab_about, text='IoT version: ' + use_api('http://localhost:4061/iot/about')
                                       .json()['libVersion'])
-        self.label_quantumleap_ver = tk.Label(main_frame,
+        self.label_quantumleap_ver = tk.Label(self.tab_about,
                                               text='QuantumLeap version: ' +
                                                    use_api('http://localhost:8668/v2/version').json()['version'])
-
         self.label_orion_ver.place(x=10, y=10)
         self.label_iot_ver.place(x=10, y=30)
         self.label_quantumleap_ver.place(x=10, y=50)
 
-        self.listbox_devices = tk.Listbox(main_frame, height=5, width=20)
-        self.listbox_devices.place(x=200, y=90)
+    def set_applications_keys_widgets(self) -> None:
 
-        self.create_device = tk.Button(main_frame, text='Create device and entity',
-                                       command=self.api_create_device_and_entity)
-        self.delete_device = tk.Button(main_frame, text='Delete device and entities',
-                                       command=self.api_delete_device_and_entity)
-        self.create_device.place(x=10, y=90)
-        self.delete_device.place(x=10, y=140)
+        # Labels
+        self.label_ttn_app_id = tk.Label(self.tab_app_keys, text='TTN Application ID')
+        self.label_ttn_app_pw = tk.Label(self.tab_app_keys, text='TTN Application Access Key')
+        self.label_ttn_app_eui = tk.Label(self.tab_app_keys, text='TTN Application EUI')
+        self.label_ttn_app_id.place(x=10, y=10)
+        self.label_ttn_app_pw.place(x=10, y=50)
+        self.label_ttn_app_eui.place(x=10, y=90)
 
-        self.label_title_devices = tk.Label(main_frame, text='DEVICES')
-        self.label_title_entities = tk.Label(main_frame, text='ENTITIES')
-        self.label_title_subscriptions = tk.Label(main_frame, text='SUBSCRIPTIONS')
+        # Inputs
+        self.entry_ttn_app_id = tk.Entry(self.tab_app_keys)
+        self.entry_ttn_app_pw = tk.Entry(self.tab_app_keys)
+        self.entry_ttn_app_eui = tk.Entry(self.tab_app_keys)
+        self.entry_ttn_app_id.place(x=10, y=30)
+        self.entry_ttn_app_pw.place(x=10, y=70)
+        self.entry_ttn_app_eui.place(x=10, y=110)
 
-        self.label_devices = tk.Label(main_frame)
-        self.label_entities = tk.Label(main_frame)
-        self.label_subscriptions = tk.Label(main_frame)
+        # Button
+        self.set_app_keys = tk.Button(self.tab_app_keys, text='Update Keys', command=self.update_app_keys)
+        self.set_app_keys.place(x=10, y=150)
 
-        self.label_title_devices.place(x=10, y=190)
-        self.label_title_entities.place(x=100, y=190)
-        self.label_title_subscriptions.place(x=190, y=190)
+    def set_devices_management_widgets(self) -> None:
+        """
+        Sets and places the widgets that are needed to manage devices
+        :return: None
+        """
+        # Listbox
+        self.listbox_devices = tk.Listbox(self.tab_devices_management, height=5, width=20)
+        self.listbox_devices.place(x=200, y=110)
+        self.listbox_devices.bind('<<ListboxSelect>>', self.enable_delete_button)
 
-        self.label_devices.place(x=10, y=210)
-        self.label_entities.place(x=100, y=210)
-        self.label_subscriptions.place(x=190, y=210)
+        # Buttons
+        self.button_create_device = tk.Button(self.tab_devices_management, text='Create device and entity',
+                                              command=self.api_create_device_and_entity)
+        self.button_delete_device = tk.Button(self.tab_devices_management, text='Delete device and entities',
+                                              command=self.api_delete_device_and_entity, state='disabled')
+        self.button_create_device.place(x=10, y=110)
+        self.button_delete_device.place(x=10, y=160)
 
-        self.api_get_devices_and_entities()
+        # Labels
+        self.label_app_skey = tk.Label(self.tab_devices_management, text='Application Session Key')
+        self.label_dev_eui = tk.Label(self.tab_devices_management, text='Device EUI')
+        self.label_title_devices = tk.Label(self.tab_devices_management, text='DEVICES')
+        self.label_title_entities = tk.Label(self.tab_devices_management, text='ENTITIES')
+        self.label_title_subscriptions = tk.Label(self.tab_devices_management, text='SUBSCRIPTIONS')
+        self.label_devices = tk.Label(self.tab_devices_management)
+        self.label_entities = tk.Label(self.tab_devices_management)
+        self.label_subscriptions = tk.Label(self.tab_devices_management)
 
-    def api_get_devices_and_entities(self):
+        self.label_app_skey.place(x=10, y=10)
+        self.label_dev_eui.place(x=10, y=50)
+        self.label_title_devices.place(x=10, y=210)
+        self.label_title_entities.place(x=100, y=210)
+        self.label_title_subscriptions.place(x=190, y=210)
+        self.label_devices.place(x=10, y=230)
+        self.label_entities.place(x=100, y=230)
+        self.label_subscriptions.place(x=190, y=230)
+
+        # Inputs
+        self.entry_app_skey = tk.Entry(self.tab_devices_management, width=33)
+        self.entry_dev_eui = tk.Entry(self.tab_devices_management, width=17)
+        self.entry_app_skey.place(x=10, y=30)
+        self.entry_dev_eui.place(x=10, y=70)
+
+    def enable_delete_button(self, event) -> None:
+        self.button_delete_device.config(state="normal")
+
+    def update_app_keys(self) -> None:
+        # get the values
+        app_id = self.entry_ttn_app_id.get()
+        app_pw = self.entry_ttn_app_pw.get()
+        app_eui = self.entry_ttn_app_eui.get().upper()
+
+        error = False
+        if len(app_id) < 3:
+            self.label_ttn_app_id.config(fg='red')
+            error = True
+        else:
+            # Update the label
+            self.label_ttn_app_id['text'] = self.label_ttn_app_id['text'].split('  ->  ')[0] + '  ->  ' + app_id
+            self.label_ttn_app_id.config(fg='black')
+        if not app_pw.startswith('ttn-account'):
+            self.label_ttn_app_pw.config(fg='red')
+            error = True
+        else:
+            # Update the label
+            self.label_ttn_app_pw['text'] = self.label_ttn_app_pw['text'].split('  ->  ')[0] + '  ->  ' + app_pw
+            self.label_ttn_app_pw.config(fg='black')
+        if len(app_eui) != 16 or not is_hex(app_eui):
+            self.label_ttn_app_eui.config(fg='red')
+            error = True
+        else:
+            # Update the label
+            self.label_ttn_app_eui['text'] = self.label_ttn_app_eui['text'].split('  ->  ')[0] + '  ->  ' + app_eui
+            self.label_ttn_app_eui.config(fg='black')
+
+        if error:
+            self.notebook.tab(1, state='disabled')
+            return
+
+        # Update the vars
+        Application.TTN_app_id = app_id
+        Application.TTN_app_pw = app_pw
+        Application.TTN_app_eui = app_eui
+
+        self.notebook.tab(1, state='normal')
+
+    def api_get_devices_and_entities(self) -> None:
+        """
+        Uses diverse APIs to retrieve data and update the application
+        :return: None
+        """
         entities = use_api('http://localhost:1026/v2/entities', header=header_orion).json()
         devices = use_api('http://localhost:4061/iot/devices', header=header_iot_device).json()['devices']
         subscriptions = use_api('http://localhost:1026/v2/subscriptions', header=header_orion).json()
@@ -122,7 +232,37 @@ class Application(tk.Frame):
         self.label_entities['text'] = '\n'.join(Application.entities)
         self.label_subscriptions['text'] = '\n'.join(Application.subscriptions)
 
-    def api_create_device_and_entity(self):
+    def api_create_device_and_entity(self) -> None:
+        """
+        Validates that the device keys are intruduceded and attacks the iot API to create a device, and then creates
+        a subscription to that device
+        :return:
+        """
+
+        app_skey = self.entry_app_skey.get()
+        dev_eui = self.entry_dev_eui.get()
+        error = False
+        if len(app_skey) != 32 or not is_hex(app_skey):
+            self.label_app_skey.config(fg='red')
+            error = True
+        else:
+            # update the label
+            self.label_app_skey['text'] = self.label_app_skey['text'].split('  ->  ')[0] + '  ->  ' + app_skey
+            self.label_app_skey.config(fg='black')
+        if len(dev_eui) != 16 or not is_hex(dev_eui):
+            self.label_dev_eui.config(fg='red')
+            error = True
+        else:
+            # Update the label
+            self.label_dev_eui['text'] = self.label_dev_eui['text'].split('  ->  ')[0] + '  ->  ' + dev_eui
+            self.label_dev_eui.config(fg='black')
+
+        if error:
+            return
+        # Update the vars
+        Application.TTN_app_skey = app_skey
+        Application.TTN_dev_eui = dev_eui
+
         # creates the device and entity
         use_api('http://localhost:4061/iot/devices', method=requests.post, header=header_iot_device,
                 body=Application.get_iot_body())
@@ -159,7 +299,7 @@ class Application(tk.Frame):
         use_api('http://localhost:1026/v2/subscriptions', method=requests.post, header=header_iot_device, body=body)
         self.api_get_devices_and_entities()
 
-    def api_delete_device_and_entity(self):
+    def api_delete_device_and_entity(self) -> None:
         device = self.listbox_devices.get(tk.ACTIVE)
         if device:
             index = Application.devices.index(device)
@@ -171,10 +311,11 @@ class Application(tk.Frame):
             use_api(f'http://localhost:4061/iot/devices/{device}', method=requests.delete, header=header_iot_device)
             use_api(f'http://localhost:1026/v2/subscriptions/{subscription}', method=requests.delete,
                     header=header_orion)
+        self.button_delete_device.config(state='disabled')
         self.api_get_devices_and_entities()
 
     @staticmethod
-    def get_iot_body():
+    def get_iot_body() -> dict:
         if len(Application.devices) == 0:
             device = Application.default_device
         else:
@@ -207,14 +348,14 @@ class Application(tk.Frame):
                         "lorawan": {
                             "application_server": {
                                 "host": "eu.thethings.network",
-                                "username": f"{TTN_app_id}",
-                                "password": f"{TTN_app_pw}",
+                                "username": f"{Application.TTN_app_id}",
+                                "password": f"{Application.TTN_app_pw}",
                                 "provider": f"TTN"
                             },
-                            "dev_eui": f"{TTN_dev_eui}",
-                            "app_eui": f"{TTN_app_eui}",
-                            "application_id": f"{TTN_app_id}",
-                            "application_key": f"{TTN_app_skey}"
+                            "dev_eui": f"{Application.TTN_dev_eui}",
+                            "app_eui": f"{Application.TTN_app_eui}",
+                            "application_id": f"{Application.TTN_app_id}",
+                            "application_key": f"{Application.TTN_app_skey}"
                         }
                     }
                 }
